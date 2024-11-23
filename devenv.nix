54be4d90384deb6b24023cfe3939c50facb36704
developomp <developomp@protonmail.com>
2024-11-23 11:21:45 +0900

    add vscode settings and its dependencies

--- devenv.nix
+++ devenv.nix
@@ -1,1 +1,18 @@
-{ ... }: {}
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    nixfmt-rfc-style
+  ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    # https://github.com/cachix/devenv/blob/main/src/modules/languages/nix.nix
+    # https://devenv.sh/supported-languages/nix/
+    nix = {
+      enable = true;
+      lsp.package = pkgs.nixd;
+    };
+  };
+}
