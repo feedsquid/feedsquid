67e71758a2a1af9d4638bad2eff3634ffa7341fd
developomp <developomp@protonmail.com>
2024-12-24 16:59:48 +0900

    add DuckDB

--- devenv.nix
+++ devenv.nix
@@ -4,7 +4,14 @@
   # https://devenv.sh/packages/
   packages = with pkgs; [
     nixfmt-rfc-style
+
+    # for DuckDB
+    stdenv.cc.cc.lib
   ];
+
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ];
+  };
 
   # https://devenv.sh/languages/
   languages = {
