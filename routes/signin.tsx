fa6aa1af907b6ca442a0636e1d3c3c6c5cd5dde0
developomp <developomp@protonmail.com>
2024-12-25 19:49:03 +0900

    prevent signing in when user's already authenticated

--- routes/signin.tsx
+++ routes/signin.tsx
@@ -1,4 +1,19 @@
 import { Head } from "$fresh/runtime.ts"
+import { Handlers } from "$fresh/server.ts"
+import { getCookies } from "$std/http/cookie.ts"
+import { env } from "~/env.ts"
+
+export const handler: Handlers = {
+  GET(req, ctx) {
+    if (getCookies(req.headers).rBucketAuth === env.PASSWORD) {
+      const url = new URL(req.url)
+      url.pathname = "/"
+      return Response.redirect(url)
+    }
+
+    return ctx.render!()
+  },
+}
 
 export default () => {
   return (
