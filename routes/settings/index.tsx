650d525f317392c65ac298d75c7b7df0aebe47a7
developomp <developomp@protonmail.com>
2024-12-25 13:05:01 +0900

    make page title represent current path

--- routes/settings/index.tsx
+++ routes/settings/index.tsx
@@ -1,14 +1,13 @@
 import { Handlers } from "$fresh/server.ts"
 import { getCookies } from "$std/http/cookie.ts"
+import { env } from "~/env.ts"
 
+import { Head } from "$fresh/runtime.ts"
 import FaArrowLeft from "@preact-icons/fa/FaArrowLeft"
-
 import Account from "~/routes/settings/(_components)/Account.tsx"
 import Theme from "~/routes/settings/(_components)/Theme.tsx"
 import Language from "~/routes/settings/(_components)/Language.tsx"
 import Debug from "~/routes/settings/(_components)/Debug.tsx"
-
-import { env } from "~/env.ts"
 
 export const handler: Handlers = {
   GET(req, ctx) {
@@ -25,6 +24,10 @@ export const handler: Handlers = {
 export default () => {
   return (
     <>
+      <Head>
+        <title>rBucket | Settings</title>
+      </Head>
+
       <nav class="flex items-center mx-auto w-full max-w-5xl p-2 gap-2">
         <a href="/" class="flex items-center p-3 hover:bg-slate-800 rounded">
           <FaArrowLeft />
--- routes/signin.tsx
+++ routes/signin.tsx
@@ -1,4 +1,5 @@
 import { type Handlers } from "$fresh/server.ts"
+import { Head } from "$fresh/runtime.ts"
 
 export const handler: Handlers = {
   GET(_req, ctx) {
@@ -8,26 +9,31 @@ export const handler: Handlers = {
 
 export default () => {
   return (
-    <form
-      class="flex size-full items-center justify-center"
-      method="post"
-      action="/api/auth/signin"
-    >
-      <div class="flex flex-col gap-2 w-80">
-        <label for="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          class="bg-slate-700 rounded h-10 px-4"
-        />
-        <button
-          type="submit"
-          class="flex items-center justify-center h-10 w-full bg-slate-100 text-slate-950 hover:bg-slate-200 active:bg-slate-300 rounded"
-        >
-          Login
-        </button>
-      </div>
-    </form>
+    <>
+      <Head>
+        <title>rBucket | Login</title>
+      </Head>
+      <form
+        class="flex size-full items-center justify-center"
+        method="post"
+        action="/api/auth/signin"
+      >
+        <div class="flex flex-col gap-2 w-80">
+          <label for="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            class="bg-slate-700 rounded h-10 px-4"
+          />
+          <button
+            type="submit"
+            class="flex items-center justify-center h-10 w-full bg-slate-100 text-slate-950 hover:bg-slate-200 active:bg-slate-300 rounded"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </>
   )
 }
