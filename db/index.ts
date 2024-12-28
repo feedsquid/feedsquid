871cb9632b5f71b0f69681d4af28c81e8f657564
developomp <developomp@protonmail.com>
2024-12-28 14:59:50 +0900

    add tag API

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,11 @@
 import { type DuckDBConnection, DuckDBInstance } from "@duckdb/node-api"
 
-let instance: DuckDBInstance | undefined
-let connection: DuckDBConnection | undefined
+// not using `DuckDB* | undefined` because initDB would have initialized them by
+// the point they're used
+export let instance: DuckDBInstance
+export let connection: DuckDBConnection
 
-const ID_SIZE = 16
+export const ID_SIZE = 16
 
 export async function initDB() {
   console.debug("initializing DB...")
@@ -12,15 +14,21 @@ export async function initDB() {
     instance = await DuckDBInstance.create("feedsquid.duckdb")
     connection = await instance.connect()
 
-    connection.run(`CREATE TABLE IF NOT EXISTS bookmarks (
-    id     VARCHAR(${ID_SIZE}) PRIMARY KEY,        -- bookmark nanoid
-    name   TEXT,                                   -- name of the bookmark
-    type   UTINYINT NOT NULL,                      -- 0 if bookmark, 1 if folder
-    parent VARCHAR(${ID_SIZE}),                    -- id of folder it belongs to
-    url    TEXT CHECK (type = 0),                  -- URL of the bookmark
-    tags   VARCHAR(${ID_SIZE})[] CHECK (type = 0), -- tag IDs
+    await connection.run(`CREATE TABLE IF NOT EXISTS bookmarks (
+    id     VARCHAR(${ID_SIZE}) PRIMARY KEY, -- bookmark nanoid
+    name   TEXT NOT NULL,                   -- name of the bookmark
+    parent VARCHAR(${ID_SIZE}),             -- nanoID of the parent bookmark
+    url    TEXT CHECK (url != ''),          -- URL of the bookmark. Can not be empty string (use NULL instead).
+    tags   VARCHAR(${ID_SIZE})[],           -- tag nanoIDs
 );`)
-  } catch {
+
+    await connection.run(`CREATE TABLE IF NOT EXISTS tags (
+  id     VARCHAR(${ID_SIZE}) PRIMARY KEY, -- tag nanoid
+  name   TEXT NOT NULL,                   -- name of the tag
+  parent VARCHAR(${ID_SIZE}),             -- nanoID of the parent tag
+);`)
+  } catch (err) {
+    console.error(err)
     console.error("failed to initialize DB! Terminating...")
     Deno.exit(1)
   }
---
+++ db/tag.ts
@@ -0,0 +1,104 @@
+import type { DuckDBMaterializedResult } from "@duckdb/node-api"
+
+import { connection, ID_SIZE } from "~/db/index.ts"
+import { nanoid } from "nanoid"
+import { Result } from "~/util.ts"
+
+export type TagID = string
+
+/**
+ * Tag data saved in DB
+ */
+export type Tag = {
+  id: TagID
+  name: string
+  parent: TagID | null
+}
+
+export async function listTags(): Promise<Result<Tag[]>> {
+  const queryResult = await connection.run("SELECT * FROM tags;")
+  const rows = await queryResult.getRows()
+  const data: Tag[] = []
+
+  for (const [id, name, parent] of rows) {
+    if (typeof id !== "string") {
+      return {
+        success: false,
+        reason: `tag id "${id}" is not type of string`,
+      }
+    }
+
+    if (typeof name !== "string") {
+      return {
+        success: false,
+        reason: `tag name "${name}" is not type of string`,
+      }
+    }
+
+    if (typeof parent !== "string" && parent !== null) {
+      return {
+        success: false,
+        reason: `tag parent "${parent}" is not type of string or null`,
+      }
+    }
+
+    data.push({ id, name, parent })
+  }
+
+  return { success: true, data }
+}
+
+export async function createTag(
+  { name, parent }: Pick<Tag, "name" | "parent">,
+) {
+  const prepared = await connection.prepare(
+    "INSERT INTO tags VALUES ($1, $2, $3);",
+  )
+  prepared.bindVarchar(1, nanoid(ID_SIZE))
+  prepared.bindVarchar(2, name)
+  if (parent === null) {
+    prepared.bindNull(3)
+  } else {
+    prepared.bindVarchar(3, parent)
+  }
+
+  return await prepared.run()
+}
+
+export async function removeTag(
+  id: Tag["id"],
+): Promise<Result<DuckDBMaterializedResult>> {
+  // mmm, race condition
+  const directChildrenQuery = await connection.prepare(
+    "SELECT * FROM tags WHERE parent = $1;",
+  )
+  directChildrenQuery.bindVarchar(1, id)
+  const directChildren = await directChildrenQuery.run()
+    .then(
+      (result) => result.getRows(),
+    )
+
+  for (const [id] of directChildren) {
+    if (typeof id !== "string") {
+      return { success: false, reason: `tag id "${id}" is not type of string` }
+    }
+
+    await removeTag(id)
+  }
+
+  const deletionQuery = await connection.prepare(
+    "DELETE FROM tags WHERE id = $1;",
+  )
+  deletionQuery.bindVarchar(1, id)
+  const rows = await deletionQuery.run().then((result) => result.getRows())
+
+  if (rows[0][0] === 1n) {
+    return { success: true }
+  }
+
+  if (rows[0][0] === 0n) {
+    return { success: false, reason: `tag with id "${id}" was not found` }
+  }
+
+  return { success: false, reason: "unknown" }
+}
